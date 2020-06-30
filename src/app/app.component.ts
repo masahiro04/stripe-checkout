48ea79df0ec2ccc206446302be90695fccbab69e
Masahiro Okubo <stmhamachiii@gmail.com>
2020-06-30 23:20:06 +0900

    initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,37 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'stripe-checkout';
+export class AppComponent implements OnInit {
+    title = 'stripe-checkout';
+    public stripekey: string = 'pk_~~~~';
+    public handler: any;
+
+    public ngOnInit() {
+        // イベント
+        $('#btn-register-card').on('click', () => {
+            this.openHandler();
+        });
+        this.handler = StripeCheckout.configure({
+            key: this.stripekey,
+            locale: 'ja',
+            opened: () => {},
+            token: (source) => {
+                console.log(source);
+            }
+        });
+    }
+
+    public openHandler() {
+        this.handler.open({
+            name: '大久保将広',
+            description: 'クレジットカードの変更',
+            email: 'info@masahiro.me',
+            allowRememberMe: false,
+            panelLabel: '変更'
+        });
+    }
 }
